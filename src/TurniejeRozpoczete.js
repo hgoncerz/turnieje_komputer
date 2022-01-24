22e29454bc820cb9fdd23e4855c2d1c65ca9a7c5
hgoncerz <80000293+hgoncerz@users.noreply.github.com>
2022-01-24 18:51:45 +0100

    dodanie uczestnika

--- src/TurniejeRozpoczete.js
+++ src/TurniejeRozpoczete.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useEffect } from "react";
+import { nanoid } from "nanoid";
+import ReadOnlyRow from "./components/ReadOnlyRow";
 
 const TurniejeRozpoczete = () => {
   const [imie, setImie] = useState();
@@ -11,12 +13,81 @@ const TurniejeRozpoczete = () => {
   const [data, setData] = useState();
   const [miejsce, setMiejsce] = useState();
   const [grupa, setGrupa] = useState();
-  const [test, setTest] = useState();
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   let zawodnik = [];
   const [zawodnik1, setZawodnik1] = useState();
+  const [zawodnicyBaza, setZawodnicyBaza] = useState();
   let licznik = 1;
+  const [dodajDataForm, setDodajDataForm] = useState({
+    imie: "",
+    nazwisko: "",
+    elo: "",
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/zawodnicy`)
+      .then((response) => response.json())
+      .then((data) => {
+        setZawodnicyBaza(data);
+      });
+  }, []);
+
+  const handleAddFormChange = (e) => {
+    e.preventDefault();
+
+    const zawodnikRow = e.target.getAttribute("name");
+    const zawodnikWartosc = e.target.value;
+
+    const nowyDataForm = { ...dodajDataForm };
+    nowyDataForm[zawodnikRow] = zawodnikWartosc;
+
+    setDodajDataForm(nowyDataForm);
+  };
+
+  const handleAddFormSubmit = (e) => {
+    e.preventDefault();
+    let eloWyswietl = "0";
+
+    let nowyZawodnik1;
+
+    let czyJestTaki = false;
+    const sprawdzenie = zawodnicyBaza.map(function (zawodnik) {
+      if (
+        zawodnik.imie === dodajDataForm.imie &&
+        zawodnik.nazwisko === dodajDataForm.nazwisko
+      ) {
+        czyJestTaki = true;
+        eloWyswietl = zawodnik.elo;
+      }
+    });
+    if (czyJestTaki) {
+      console.log("jest taki i elo to: " + eloWyswietl);
+      const nowyZawodnik = {
+        id: nanoid(),
+        imie: dodajDataForm.imie,
+        nazwisko: dodajDataForm.nazwisko,
+        punkty: "0",
+        ilosc_meczy: "0",
+        elo: eloWyswietl,
+      };
+      nowyZawodnik1 = [...zawodnik1, nowyZawodnik];
+    } else {
+      nowyZawodnik1 = [...zawodnik1];
+      console.log("nie dodano bo nie ma w bazie");
+    }
+
+    console.log(zawodnicyBaza);
+    setZawodnik1(nowyZawodnik1);
+  };
+
+  const handleDeleteClick = (zawodnikId) => {
+    const nowyZawodnik1 = [...zawodnik1];
+    const index = zawodnik1.findIndex((zawodnik) => zawodnik.id === zawodnikId);
+    nowyZawodnik1.splice(index, 1);
+    setZawodnik1(nowyZawodnik1);
+  };
+
   const rozdzielDane = () => {
     let imie_array = imie.split(" ");
     imie_array.splice(0, 1);
@@ -95,52 +166,59 @@ const TurniejeRozpoczete = () => {
           <h2>Miejsce: {miejsce}</h2>
           <h2>Grupa: {grupa}</h2>
           <br></br>
-          <table>
-            <thead>
-              <tr>
-                <th>Poz.</th>
-                <th>Imie</th>
-                <th>Nazwisko</th>
-                <th>Punkty</th>
-                <th>Mecze</th>
-                <th>ELO</th>
-              </tr>
-            </thead>
-            <tbody>
-              {zawodnik1.map((zawodnik) => (
-                <tr>
-                  <td>{licznik++}</td>
-                  <td>{zawodnik.imie}</td>
-                  <td>{zawodnik.nazwisko}</td>
-                  <td>{zawodnik.punkty}</td>
-                  <td>{zawodnik.ilosc_meczy}</td>
-                  <td>{zawodnik.elo}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h2>Dodaj nowego uczestnika</h2>
           <form>
+            <table>
+              <thead>
+                <tr>
+                  <th>Poz.</th>
+                  <th>Imie</th>
+                  <th>Nazwisko</th>
+                  <th>Punkty</th>
+                  <th>Mecze</th>
+                  <th>ELO</th>
+                  <th>Akcja</th>
+                </tr>
+              </thead>
+              <tbody>
+                {zawodnik1.map((zawodnik) => (
+                  <tr>
+                    <td>{licznik++}</td>
+                    <td>{zawodnik.imie}</td>
+                    <td>{zawodnik.nazwisko}</td>
+                    <td>{zawodnik.punkty}</td>
+                    <td>{zawodnik.ilosc_meczy}</td>
+                    <td>{zawodnik.elo}</td>
+                    <button
+                      type="button"
+                      onClick={() => handleDeleteClick(zawodnik.id)}
+                    >
+                      Usuń
+                    </button>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </form>
+          <h2>Dodaj nowego uczestnika</h2>
+          <a>Użytkownik musi być w bazie zawodników</a>
+          <form onSubmit={handleAddFormSubmit}>
             <input
               type="text"
               name="imie"
               required="required"
               placeholder="Wprowadź imie"
+              onChange={handleAddFormChange}
             ></input>
             <input
               type="text"
               name="nazwisko"
               required="required"
               placeholder="Wprowadź nazwisko"
-            ></input>
-            <input
-              type="text"
-              name="elo"
-              required="required"
-              placeholder="Wprowadź elo"
+              onChange={handleAddFormChange}
             ></input>
             <button type="submit">Dodaj</button>
           </form>
+          <button>Wylosuj pary</button>
         </article>
       )}
     </div>
